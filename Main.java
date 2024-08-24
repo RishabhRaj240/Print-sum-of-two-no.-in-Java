13f4182c4f6aa752670738cbcbc0930ad70e75eb
Rishabh Raj <rishabhraj240405@gmail.com>
2024-08-24 21:17:46 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        //int result = num1 % num2;
+        //System.out.println(result);
+        num1 *= 2;
+        num2 /= 4;
+
+        System.out.println("num1: " + num1);
+        System.out.println("num2: " + num2);
+    }
+}
